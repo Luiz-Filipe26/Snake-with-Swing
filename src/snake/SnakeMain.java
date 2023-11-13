36b83bcb02d40b4dd4b1247660ca3ee6fe5dc66d
Luiz Filipe <luizferamos26@gmail.com>
2023-11-13 12:55:52 -0300

    teste

--- src/snake/SnakeMain.java
+++ src/snake/SnakeMain.java
@@ -4,6 +4,7 @@ public class SnakeMain {
     
     public static void main(String[] args) {
         //Inicializa as classes de View, Controller e de desenho no campo
+        
         SnakeView.getInstancia().setVisible(true);
         SnakeController snakeController = SnakeController.getInstancia();
         snakeController.adicionarSnakeView();
